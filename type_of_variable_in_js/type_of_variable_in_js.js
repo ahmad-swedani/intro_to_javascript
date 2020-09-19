3529c5ea96c8b8c3caf7c1c88643043ebe0c9771
ahmad-swedani <swedani390@gmail.com>
2020-09-19 05:44:44 +0300

    type of data with some example

---
+++ type_of_variable_in_js/type_of_variable_in_js.js
@@ -0,0 +1,62 @@
+'use strict'
+
+/*   type of var in javascript:: */
+
+// type #1(Number):
+var num1 = 5;
+var num2 = 9;
+var num3 = 20;
+
+// console.log(num1 + num2 + num3);
+// console.log(num1 * num2);
+
+////////////////////////////////////////
+
+// type #2(String):
+var name1 = 'Ahmad';
+var name2 = 'Alaa';
+var name3 = 'Emad';
+var name4 = 'Qasem';
+
+// console.log(name1);
+// console.log(name2);
+// console.log(name3);
+// console.log(name4);
+
+////////////////////////////////////////
+
+// type #3(Boolean):
+var answer1 = true;
+var answer2 = false;
+
+// console.log(answer1);
+// console.log(answer2);
+
+////////////////////////////////////////
+
+// type #4(Array):
+var list1 = [1, 2, 3, 4];
+var list2 = ['ahmad', 'alaa', 'sna\'a', true, 55];
+
+
+// console.log(list2);
+// console.log(list2[2]);
+
+var list3 = [list1, list2];
+
+// console.log(list3[1][0]);
+
+
+////////////////////////////////////////
+
+// type #5(undefined):
+// any variable is not defined 
+
+////////////////////////////////////////
+
+//who i can know what is type of specific variable??
+
+var variable = 50;
+console.log(typeof variable);
+
+
